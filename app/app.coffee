b056be482ff636df2e9fab38819818c060d7a91c
unrealsolver <solver@tut.by>
2014-05-03 19:34:27 +0300

    Autoreload via nodemon

--- app/app.coffee
+++ app/app.coffee
@@ -1,7 +1,11 @@
-module.exports =
-	onRequest: (request, response) ->
-		console.log request.method + ' ' + request.url
-		response.writeHead 200, {'Content-Type': 'text/plain'}
-		response.write 'Hello Gulp!'
-		response.end()
+http = require 'http'
 
+port = 3000
+
+onRequest = (request, response) ->
+  console.log request.method + ' ' + request.url
+  response.writeHead 200, {'Content-Type': 'text/plain'}
+  response.write 'Hello Gulp!'
+  response.end()
+
+http.createServer(onRequest).listen(port)
--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,7 @@
 var gulp = require('gulp')
 var coffee = require('gulp-coffee')
 var gutil = require('gulp-util')
-
-var http = require('http')
-var app = require('./build/app')
-
-var port = 3000
+var nodemon = require('gulp-nodemon')
 
 gulp.task('coffee', function() {
 	gulp.src('./app/*.coffee')
@@ -13,8 +9,12 @@ gulp.task('coffee', function() {
 		.pipe(gulp.dest('./build'))
 })
 
-gulp.task('server', function() {
-	http.createServer(app.onRequest).listen(port)
+gulp.task('watch', function() {
+  gulp.watch('./app/*.coffee', ['coffee'])
 })
 
-gulp.task('default', ['coffee', 'server'] )
+gulp.task('serve', function() {
+  nodemon({script: './build/app.js'})
+})
+
+gulp.task('default', ['coffee', 'watch', 'serve'] )
